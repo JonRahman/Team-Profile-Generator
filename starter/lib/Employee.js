0c74ae253a8ad02a31d658b2fe5c6ed26508bf15
JonRahman <jonrahman90@gmail.com>
2024-01-19 22:38:50 +0000

    updated employee and engineer js

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
+
+// Exporting the Employee class
+module.exports = Employee;
+
+// Question 2 - Create a new instance of the Employee class using the employee object literal provided
+const employeeObjectLiteral = {
+    name: "John Doe",
+    id: "1001",
+    email: "john.doe@company.com"
+    };
--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,10 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+        }
+}
+
+// Exporting the Engineer class so that it can be accessed by other files in our application
+module.exports = Engineer;
